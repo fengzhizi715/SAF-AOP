49ad7fbe82bbbc4508761e1e69a3403244e3bc5c
fengzhizi715 <fengzhizi715@126.com>
2017-03-09 14:09:48 +0800

    完善注释

--- saf-aop/src/main/java/com/safframework/aop/Utils.java
+++ saf-aop/src/main/java/com/safframework/aop/Utils.java
@@ -13,7 +13,7 @@ public class Utils {
 
     /**
      * 获取全局的context，也就是Application Context
-     * @return
+     * @return 返回全局的Context，只适用于android 4.0以后
      */
     @TargetApi(14)
     public static Context getContext() {
