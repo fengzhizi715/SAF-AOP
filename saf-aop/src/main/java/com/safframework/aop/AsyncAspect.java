942fc0344202dd9d7ee00991bf48ddc834c8707d
fengzhizi715 <fengzhizi715@126.com>
2017-03-15 00:36:40 +0800

    rxjava1 升级到 rxjava2

--- saf-aop/src/main/java/com/safframework/aop/AsyncAspect.java
+++ saf-aop/src/main/java/com/safframework/aop/AsyncAspect.java
@@ -7,10 +7,13 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
-import rx.Observable;
-import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
 
 /**
  * Created by Tony Shen on 16/3/23.
@@ -29,18 +32,21 @@ public class AsyncAspect {
 
     private void asyncMethod(final ProceedingJoinPoint joinPoint) throws Throwable {
 
-        Observable.create(new Observable.OnSubscribe<Object>() {
-
-            @Override
-            public void call(Subscriber<? super Object> subscriber) {
-                Looper.prepare();
-                try {
-                    joinPoint.proceed();
-                } catch (Throwable throwable) {
-                    throwable.printStackTrace();
-                }
-                Looper.loop();
-            }
-        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe();
+        Flowable.create(new FlowableOnSubscribe<Object>() {
+                            @Override
+                            public void subscribe(FlowableEmitter<Object> e) throws Exception {
+                                Looper.prepare();
+                                try {
+                                    joinPoint.proceed();
+                                } catch (Throwable throwable) {
+                                    throwable.printStackTrace();
+                                }
+                                Looper.loop();
+                            }
+                        }
+                , BackpressureStrategy.BUFFER)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe();
     }
 }
